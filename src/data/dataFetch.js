c847e2c9fbbc5c8e5895ece0c7bdb1537750db45
djriad157764-creator <djriad157764@gmail.com>
2026-04-29 17:42:02 +0600

    Fix: Issue

--- src/data/dataFetch.js
+++ src/data/dataFetch.js
@@ -1,9 +1,4 @@
-// https://pix-gen-omega.vercel.app/data.json
-
-// https://pix-gen-omega.vercel.app/category.json
-
 export const fetchData = async () => {
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
   const res = await fetch("https://pix-gen-omega.vercel.app/data.json");
   const data = await res.json();
   return data;
